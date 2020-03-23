3f4d221d9bde532ad3c386ace2489b617d9822cd
Adriana Moreira <adrianamoreiraadm@gmail.com>
2020-03-23 20:50:09 +0000

    creating buttons

--- Patterns/src/patterns/Frame.java
+++ Patterns/src/patterns/Frame.java
@@ -1,6 +1,18 @@
 package patterns;
 
-public class Frame {
+public class Frame extends JFrame{
 	
+	
+	public Frame() {
+		
+		
+		//Creating Buttons
+		button1 = new JButton ("All Countries");
+		button2 = new JButton ("By Code");
+		button3 = new Jbutton ("By name");
+		lbMenu = new JLabel ("Please, choose an option to get the records: ");
+	
+	
+	}
 
 }
