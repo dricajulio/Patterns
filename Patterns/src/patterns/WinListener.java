bc5b2f58dc82debc3acb72066829681eb0ea736d
Adriana Moreira <adrianamoreiraadm@gmail.com>
2020-03-31 14:15:33 +0100

    Connecting and Disconnecting from the database when the events start and close the screen.

--- Patterns/src/patterns/WinListener.java
+++ Patterns/src/patterns/WinListener.java
@@ -5,54 +5,52 @@ import java.awt.event.WindowListener;
 
 public class WinListener implements WindowListener{
 
+	// Each instance of this class will have a 
+	// a reference to the singleton writer object
 	SingletonWritter sw = SingletonWritter.getInstance ();
-
+	
+	
 @Override
-public void windowActivated(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-@Override
-public void windowClosed(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-@Override
-public void windowClosing(WindowEvent e) {
-	// TODO Auto-generated method stub
-	System.exit(0);
-	
-}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+	}
 
-@Override
-public void windowDeactivated(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// Using the singleton writer object
+		sw.DisconnectDatabase();
+		System.exit(0);
+	}
 
-@Override
-public void windowDeiconified(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-//@Override
-public void windowconified(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-@Override
-public void windowOpened(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-@Override
-public void windowIconified(WindowEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		// Using the singleton writer
+		sw.ConnectToDatabase();
+	}
 
 }
